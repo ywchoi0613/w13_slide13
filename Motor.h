2a59863f00d53e86fe55f016a8146ba7e3c68105
ywchoi0613 <127080350+ywchoi0613@users.noreply.github.com>
2023-05-31 16:45:41 +0900

    Create Motor.h

---
+++ Motor.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Car.h"
+class Motor : public Car {
+private:
+	int spareWheels;
+	int helmets;
+public:
+	void klaxon(int);
+	void setSparewheels(int);
+	int getSparewheels();
+	float getTotalPrice();
+};
