538b63af2252a173bed9c6febef5fc8a15dc0300
ywchoi0613 <127080350+ywchoi0613@users.noreply.github.com>
2023-05-31 16:46:06 +0900

    Create SUV.h

---
+++ SUV.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Car.h"
+class SUV : public Car {
+private:
+	int spareWheels;
+public:
+	void klaxon(int);
+	void setSparewheels(int);
+	int getSparewheels();
+	float getTotalPrice();
+};
